be1209bd1ef64ea217bfd5042c86d8fd4da51b7c
Caleb <Tigers1025@gmail.com>
2022-08-23 11:11:56 -0400

    JS Update

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
     .then(resp => resp.json())
-    .then(resp => console.log(resp))
+    .then(data =>{
+       data.drinks.forEach(drink =>{renderDrinks(drink)})
+    })
+
+    function renderDrinks(drink){
+        let li = document.createElement('li')
+        let list = document.querySelector('#nameList')
+
+        li.textContent = drink.strDrink
+
+        list.append(li)
+    }
