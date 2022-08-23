2ee0a487dbcec0828ef002932df20001459e1aa1
Caleb <Tigers1025@gmail.com>
2022-08-23 11:52:31 -0400

    More things

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
     .then(resp => resp.json())
     .then(data =>{
        data.drinks.forEach(drink =>{renderDrinks(drink)})
+       renderDrinkDetails(data.drinks[0])
     })
 
     function renderDrinks(drink){
@@ -11,4 +12,25 @@ fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
         li.textContent = drink.strDrink
 
         list.append(li)
+
+        li.addEventListener('click', ()=>{
+            renderDrinkDetails(drink)
+        })
+    }
+
+    function renderDrinkDetails(drink){
+        const title = document.querySelector('#title')
+        const img = document.querySelector('#img')
+        const directions = document.querySelector('#directions')
+        const ingredList = document.querySelector('#ingredients')
+        const li = document.createElement('li')
+
+        title.textContent = drink.strDrink
+        img.src = drink.strDrinkThumb
+        directions.textContent = drink.strInstructions
+
+
+
+
+
     }
