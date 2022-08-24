15791b01b23aa129c0af974618523013160b390d
Caleb <Tigers1025@gmail.com>
2022-08-24 12:21:43 -0400

    Final Touches

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
        data.drinks.forEach(drink =>{renderDrinks(drink)})
        renderDrinkDetails(data.drinks[0])
     })
-
+// Adds names to the top of the page
     function renderDrinks(drink){
         let li = document.createElement('li')
         let list = document.querySelector('#nameList')
@@ -12,27 +12,25 @@ fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
         li.textContent = drink.strDrink
 
         list.append(li)
-
+//click event to render drink details to page
         li.addEventListener('click', ()=>{
             document.getElementById("ingredients").innerHTML=""
             document.getElementById("ingredients").textContent = 'Ingredients'
             renderDrinkDetails(drink)
         })
     }
-
+// Renders details to page
     function renderDrinkDetails(drink){
         const title = document.querySelector('#title')
         const img = document.querySelector('#img')
         const directions = document.querySelector('#directions')
-        const li = document.createElement('li')
-
+        
         title.textContent = drink.strDrink
         img.src = drink.strDrinkThumb
         directions.textContent = drink.strInstructions
 
         let inglist = document.getElementById("ingredients")
-        let ing = document.createElement("li")
-
+      
         const one = document.createElement('li')
         const two = document.createElement('li')
         const three = document.createElement('li')
@@ -40,8 +38,8 @@ fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
         const five = document.createElement('li')
         const six = document.createElement('li')
         const seven = document.createElement('li')
-    
-    
+
+    //Callback function to add ingredients in a list
         ingr (drink.strIngredient1, drink.strMeasure1, one)
         ingr (drink.strIngredient2, drink.strMeasure2, two)
         ingr (drink.strIngredient3, drink.strMeasure3, three)
@@ -54,11 +52,9 @@ fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
         const btn = document.querySelector('#like')
 
         btn.addEventListener('click', ()=> handleButton())
-
-        
-
     }
 
+//Handles the like button
     function handleButton(){
         const btn = document.querySelector('#like')
         if(btn.textContent=="Yummy"){
@@ -66,18 +62,13 @@ fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
         else if(btn.textContent=="Not Yummy"){
             btn.textContent="Yummy";}
     }
-
-
-
-
-
-
+// Kenneth's magic function
     function ingr (ing, measurement, num) {
         if (ing === null) {num.textContent = ""}
          else if (measurement === null) {num.textContent = `${ing}`}    
             else {num.textContent = `${ing} ${measurement}`}
     }
-
+// Submit function
 const form = document.querySelector('#form')
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
@@ -91,5 +82,17 @@ form.addEventListener('submit', (e)=>{
 
 
 })
+// Spicy Surprise
+document.addEventListener('keydown', (e)=>{
+   let key = e.key
+    if (key === 'ArrowLeft'){
+        document.querySelector('#document').innerHTML = "GO HOME YOU'RE DRUNK!!!!!"
+    }
+})
+
+
+
+
+
 
 
