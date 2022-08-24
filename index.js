171f3ea2b8df01503a21f68f1d7a625ceb19a778
Kenneth Latour <kenneth.latourii@gmail.com>
2022-08-24 09:13:19 -0500

    Javascript ing.

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
         list.append(li)
 
         li.addEventListener('click', ()=>{
+            document.getElementById("ingredients").innerHTML=""
             renderDrinkDetails(drink)
         })
     }
@@ -22,15 +23,43 @@ fetch ('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
         const title = document.querySelector('#title')
         const img = document.querySelector('#img')
         const directions = document.querySelector('#directions')
-        const ingredList = document.querySelector('#ingredients')
         const li = document.createElement('li')
 
         title.textContent = drink.strDrink
         img.src = drink.strDrinkThumb
         directions.textContent = drink.strInstructions
 
+        let inglist = document.getElementById("ingredients")
+        let ing = document.createElement("li")
 
-
-
+        const one = document.createElement('li')
+        const two = document.createElement('li')
+        const three = document.createElement('li')
+        const four = document.createElement('li')
+        const five = document.createElement('li')
+        const six = document.createElement('li')
+        const seven = document.createElement('li')
+    
+    
+        ingr (drink.strIngredient1, drink.strMeasure1, one)
+        ingr (drink.strIngredient2, drink.strMeasure2, two)
+        ingr (drink.strIngredient3, drink.strMeasure3, three)
+        ingr (drink.strIngredient4, drink.strMeasure4, four)
+        ingr (drink.strIngredient5, drink.strMeasure5, five)
+        ingr (drink.strIngredient6, drink.strMeasure6, six)
+        ingr (drink.strIngredient7, drink.strMeasure7, seven)
+        
+        inglist.append(one, two, three, four, five, six, seven)
 
     }
+
+    function ingr (ing, measurement, num) {
+        if (ing === null) {num.textContent = ""}
+         else if (measurement === null) {num.textContent = `${ing}`}    
+            else {num.textContent = `${ing} ${measurement}`}
+    }
+
+
+
+
+    
